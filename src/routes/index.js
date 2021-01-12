2e0e630803aaab94f2bea87d92226ebbbf13154f
Jackson Lawrence <jlawrence1787@gmail.com>
2021-01-12 08:00:15 -0800

    Redesign

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const jsonfile = require('jsonfile')
-
 
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-    jsonfile.readFile('./bin/json/projects.json', (e,i) => {
-        if(e) console.log(e);
-        res.render('index', {projects: JSON.stringify(i)})
-    })
+    res.render('index')
 });
 
 module.exports = router;
