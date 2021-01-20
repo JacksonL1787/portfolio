443df2218a8218d6c4018e6ce4ba5a22b1bec37c
Jackson Lawrence <jlawrence1787@gmail.com>
2021-01-19 20:36:08 -0800

    Added Contact Form

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,13 +1,13 @@
 $("#about-nav-link").click(() => {
     $("body").animate({
         scrollTop: $("#main")[0].offsetTop + $("#main .about-me-container")[0].offsetTop - 50
-    }, 500)
+    }, 400)
 })
 
 $("#scroll-down-button").click(() => {
     $("body").animate({
         scrollTop: $("#main")[0].offsetTop - 50
-    }, 500)
+    }, 400)
 })
 
 $("#projects-nav-link, .view-projects-button").click(() => {
@@ -16,10 +16,27 @@ $("#projects-nav-link, .view-projects-button").click(() => {
     }, 500)
 })
 
-$("#contact-me-container .form-container input").focusin(function() {
+$("#nav .lets-talk-button, .about-me-container .contact-me-link").click(() => {
+    $("body").animate({
+        scrollTop: $("#main")[0].offsetTop + $("#contact-container")[0].offsetTop - 50
+    }, 600)
+})
+
+$("#contact-container .input-container").click(function() {
+    $(this).find("input, textarea").focus()
+})
+
+$("#contact-container .label").click(function(e) {
+    e.preventDefault()
+})
+
+$("#contact-container input, #contact-container textarea").focusin(function() {
     $(this).parent().addClass("active")
 })
 
-$("#contact-me-container .form-container input").focusout(function() {
-    $("#contact-me-container .form-container .input-container").removeClass("active")
+$("#contact-container input, #contact-container textarea").focusout(function() {
+    if($(this).val().trim().length > 0) return;
+    $(this).parent().removeClass("active")
 })
+
+
